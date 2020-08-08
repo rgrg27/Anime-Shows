5150f8dca2e565f67b9535d5271fa56ffa0ef443
Rahul Garg <rgrg27@gmail.com>
2020-08-09 03:14:44 +0530

    Updates

---
+++ precache-manifest.ab699c4f12ec2ffab804c19149d11524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c272cbf58c584f27e2a690470a3fc1c",
+    "url": "/Anime-Shows/index.html"
+  },
+  {
+    "revision": "bccec045679c106e8373",
+    "url": "/Anime-Shows/static/css/main.e9deb4a6.chunk.css"
+  },
+  {
+    "revision": "e56246f897b3ad181915",
+    "url": "/Anime-Shows/static/js/2.c987a206.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Anime-Shows/static/js/2.c987a206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccec045679c106e8373",
+    "url": "/Anime-Shows/static/js/main.9de013f3.chunk.js"
+  },
+  {
+    "revision": "2ebf087324c2a85d4def",
+    "url": "/Anime-Shows/static/js/runtime-main.56c3bc0f.js"
+  }
+]);
